d5c9c51c6f44f7d3eede963dc17237def4aad163
sridharsridhar1372-code <sridharsridhar1372@gmail.com>
2026-09-09 10:11:56 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,16 @@
+CREATE TABLE Student(
+   StudentID INT(5) primary key,
+   StudentName VARCHAR(20) NOT NULL,
+   DOB DATE default null,
+   Gender VARCHAR(10) NOT NULL,
+   DepartmentID  INT(5),
+   CONSTRAINT UQ_StudentID UNIQUE(StudentID),CONSTRAINT FK_Student_DepartmentID FOREIGN KEY(DepartmentID) REFERENCES Departments(DepartmentID)
+);
+DESC Student;
+SELECT*FROM Student;
+Footer
+© 2026 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
 
